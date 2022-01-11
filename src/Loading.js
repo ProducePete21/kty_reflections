93d79f16f76cc3ea17a92f1bf8c901f2aa1581ce
Produce Pete <producepete21@gmail.com>
2022-01-11 11:33:51 -0500

    Rearranged Loading Screen

--- src/Loading.js
+++ src/Loading.js
@@ -5,11 +5,14 @@ import { Grid, LinearProgress, CircularProgress, Typography } from '@mui/materia
 const Loading = ({name, id, handleChange, label, value, autoFocus, dollarSign, type}) => {
     return(
         <Grid item style={{width: '300px'}}>
-            <div style={{borderBottom: 'solid', borderBottomWidth: 'thin', borderRadius: '5px', borderColor: 'rgba(189, 195, 199, 0.9)', height: '54px', backgroundColor: 'rgba(0, 0, 0, 0.08)', marginTop: '10px', paddingTop: '10px'}}>
+            <div style={{borderBottom: 'solid', borderBottomWidth: 'thin', borderRadius: '5px', borderColor: 'rgba(189, 195, 199, 0.9)', height: '80px', backgroundColor: 'rgba(0, 0, 0, 0.08)', marginTop: '20px', paddingTop: '10px'}}>
+                <LinearProgress style={{left: '8px', width: '95%', paddingBottom: '10px'}}/>
                 <Typography align='center' style={{marginTop: '10px'}}>
                     Waiting for data to load!
                 </Typography>
-                <LinearProgress style={{top: '25px', left: '8px', width: '95%', paddingBottom: '10px'}}/>
+                <Typography align='center' variant='subtitle2'>
+                    (Be Patient! It's a lot of info!)
+                </Typography>
             </div>
         </Grid>
     )
